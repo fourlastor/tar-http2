caa118b081da87df9b5d5a3a9f8e5b9398357850
Daniele Conti <daniele.conti@novoda.com>
2018-04-18 20:31:46 +0800

    Add benchmark running

--- app/src/main/java/com/example/tarvshttp2/MainActivity.kt
+++ app/src/main/java/com/example/tarvshttp2/MainActivity.kt
@@ -1,12 +1,55 @@
 package com.example.tarvshttp2
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.TextView
+import com.facebook.battery.metrics.cpu.CpuMetrics
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val subscriptions = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val repository = Repository()
+
+        button_start_tar.setOnClickListener {
+            subscriptions += measure(repository.fetchTar(), loading_tar, result_tar)
+        }
+
+        button_start_images.setOnClickListener {
+            subscriptions += measure(repository.fetchImages(), loading_images, result_images)
+        }
     }
+
+    private fun measure(source: Single<CpuMetrics>, progressBar: ProgressBar, textView: TextView): Disposable {
+        return source
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe { progressBar.visibility = View.VISIBLE }
+                .subscribe { metrics ->
+                    progressBar.visibility = View.GONE
+                    textView.text = metrics.toString()
+                }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        subscriptions.clear()
+    }
+}
+
+private operator fun CompositeDisposable.plusAssign(subscription: Disposable) {
+    add(subscription)
 }
---
+++ app/src/main/java/com/example/tarvshttp2/Repository.kt
@@ -0,0 +1,60 @@
+package com.example.tarvshttp2
+
+import com.example.tarvshttp2.BuildConfig.CLOUDFRONT_HOST
+import com.example.tarvshttp2.CloudFront.IMAGES
+import com.facebook.battery.metrics.cpu.CpuMetrics
+import com.facebook.battery.metrics.cpu.CpuMetricsCollector
+import io.reactivex.Completable
+import io.reactivex.Single
+import okhttp3.*
+import okhttp3.logging.HttpLoggingInterceptor
+import java.io.IOException
+
+class Repository {
+    private val client: OkHttpClient = OkHttpClient.Builder()
+            .addNetworkInterceptor(HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY })
+            .build()
+
+    private val collector = CpuMetricsCollector()
+
+    fun fetchTar() = metricsOn(request("tar/edition_2018-04-18_xhdpi.tar"))
+
+    fun fetchImages() = metricsOn(Completable.merge(IMAGES.map { request("images/$it") }))
+
+    private fun metricsOn(original: Completable): Single<CpuMetrics> {
+        val initialMetrics = collector.createMetrics()
+        val finalMetrics = collector.createMetrics()
+        return original
+                .doOnSubscribe { collector.getSnapshot(initialMetrics) }
+                .doOnComplete { collector.getSnapshot(finalMetrics) }
+                .toSingle { finalMetrics - initialMetrics }
+    }
+
+    private fun request(path: String) = Completable.create { emitter ->
+        val request = Request.Builder()
+                .cacheControl(CacheControl.FORCE_NETWORK)
+                .url("$CLOUDFRONT_HOST/$path")
+                .build()
+
+        val call = client.newCall(request).apply {
+            enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    emitter.onError(e)
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    if (response.protocol() != Protocol.HTTP_2) {
+                        emitter.onError(IllegalStateException("Protocol is not HTTP/2"))
+                    } else {
+                        emitter.onComplete()
+                    }
+                }
+
+            })
+        }
+
+        emitter.setCancellable { call.cancel() }
+    }
+}
+
+private operator fun CpuMetrics.minus(other: CpuMetrics): CpuMetrics = diff(other)
